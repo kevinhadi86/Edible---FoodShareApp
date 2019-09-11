4fdaeb0fb9d99f4e64e690a4113f84e2a40a4a1f
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-11 18:21:21 +0800

    add jwtAuthenticationEntryPoint

---
+++ src/main/java/edible/simple/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: JwtAuthenticationEntryPoint.java, v 0.1 2019‐09‐11 18:17 Kevin Hadinata Exp $$
+ */
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationEntryPoint.class);
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e) throws IOException, ServletException {
+        logger.error("Responding with unauthorized error. Message - {}", e.getMessage());
+        httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, e.getMessage());
+
+    }
+}
