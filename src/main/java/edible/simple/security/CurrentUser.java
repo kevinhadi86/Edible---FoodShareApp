bc8288150286814ec7d2dcac27899080341a28e9
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-11 19:22:07 +0800

    add current user annotation

---
+++ src/main/java/edible/simple/security/CurrentUser.java
@@ -0,0 +1,20 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.security;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.*;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: CurrentUser.java, v 0.1 2019‐09‐11 19:19 Kevin Hadinata Exp $$
+ */
+@Target({ElementType.PARAMETER, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@AuthenticationPrincipal
+public @interface CurrentUser {
+}
