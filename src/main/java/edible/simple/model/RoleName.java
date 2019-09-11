be03eda267dfa00398809885512f8d430a05a6ac
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-11 12:45:29 +0800

    add RoleName Enum

---
+++ src/main/java/edible/simple/model/RoleName.java
@@ -0,0 +1,14 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.model;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: RoleName.java, v 0.1 2019‐09‐11 12:40 Kevin Hadinata Exp $$
+ */
+public enum RoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
