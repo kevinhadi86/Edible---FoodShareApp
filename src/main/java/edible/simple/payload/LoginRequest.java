99ec89258f9f09660bd068358d648d1d2338b8f9
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-12 18:30:49 +0800

    add payload for login and register

---
+++ src/main/java/edible/simple/payload/LoginRequest.java
@@ -0,0 +1,36 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.payload;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: LoginRequest.java, v 0.1 2019‐09‐12 18:25 Kevin Hadinata Exp $$
+ */
+public class LoginRequest {
+
+    @NotBlank
+    private String usernameOrEmail;
+
+    @NotBlank
+    private String password;
+
+    public String getUsernameOrEmail() {
+        return usernameOrEmail;
+    }
+
+    public void setUsernameOrEmail(String usernameOrEmail) {
+        this.usernameOrEmail = usernameOrEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
