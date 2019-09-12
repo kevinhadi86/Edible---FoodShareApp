e7477e591b78293eaf5d4c16ad07e9213b6d89aa
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-12 19:24:15 +0800

    Enabling CORS

---
+++ src/main/java/edible/simple/config/WebMvcConfig.java
@@ -0,0 +1,25 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: WebMvcConfig.java, v 0.1 2019‐09‐12 19:23 Kevin Hadinata Exp $$
+ */
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final long MAX_AGE_SECS = 3600;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE")
+                .maxAge(MAX_AGE_SECS);
+    }
+}
