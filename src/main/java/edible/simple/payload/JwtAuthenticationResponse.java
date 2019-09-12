92188c2db3a4fa7cb8ce061a800cc73b5599c4eb
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-12 18:32:38 +0800

    add jwtAuthResponse

---
+++ src/main/java/edible/simple/payload/JwtAuthenticationResponse.java
@@ -0,0 +1,35 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.payload;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: JwtAuthenticationResponse.java, v 0.1 2019‐09‐12 18:31 Kevin Hadinata Exp $$
+ */
+public class JwtAuthenticationResponse {
+
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthenticationResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+}
