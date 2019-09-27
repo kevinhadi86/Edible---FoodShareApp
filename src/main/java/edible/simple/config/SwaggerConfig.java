ebbb51f09b648e865d54e8731e948ce50e00ad34
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-09-27 16:17:52 +0800

    add swagger, biar swag

---
+++ src/main/java/edible/simple/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004‐2019 All Rights Reserved.
+ */
+package edible.simple.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+/**
+ * @author Kevin Hadinata
+ * @version $Id: SwaggerConfig.java, v 0.1 2019‐09‐27 16:15 Kevin Hadinata Exp $$
+ */
+
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+
+    @Bean
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("edible.simple.controller"))
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo(
+                "Image Application",
+                "This application micro service for upload image.",
+                "1.0.0",
+                "",
+                null,
+                "",
+                "",
+                Collections.emptyList()
+        );
+    }
+}
