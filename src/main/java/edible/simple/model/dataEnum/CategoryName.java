03bd472a113cfce2c7cfb1c082e9b03381ed6300
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-11-19 14:45:44 +0800

    change kudapan to snack

--- src/main/java/edible/simple/model/dataEnum/CategoryName.java
+++ src/main/java/edible/simple/model/dataEnum/CategoryName.java
@@ -9,7 +9,7 @@ package edible.simple.model.dataEnum;
  * @version $Id: CategoryName.java, v 0.1 2019‐09‐17 18:40 Kevin Hadinata Exp $$
  */
 public enum CategoryName {
-                          KUDAPAN("Kudapan"), SAYUR("Sayur dan Buah"), BUMBU("Bumbu Dapur"), MINUMAN("Minuman Ringan"), DAGING("Daging");
+                          SNACK("Snack"), SAYUR("Sayur dan Buah"), BUMBU("Bumbu Dapur"), MINUMAN("Minuman Ringan"), DAGING("Daging");
 
     private final String toString;
 
