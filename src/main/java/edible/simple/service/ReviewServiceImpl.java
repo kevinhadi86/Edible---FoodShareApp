8718e70f3b9a6b783e025d758fd523a14028a2cc
Kevin Hadinata <wb-kevinhadi529580@antfin.com>
2019-11-21 14:32:56 +0800

    fix other user review

--- src/main/java/edible/simple/service/ReviewServiceImpl.java
+++ src/main/java/edible/simple/service/ReviewServiceImpl.java
@@ -36,7 +36,7 @@ public class ReviewServiceImpl implements ReviewService{
 
     @Override
     public List<Review> getReviewByOwner(User user) {
-        return reviewRepository.findAllByTransaction_User(user);
+        return reviewRepository.findAllByTransaction_Offer_User(user);
     }
 
     @Override
